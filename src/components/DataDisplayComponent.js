9deaa72ce7084c31c2dd35c2304eb634272f8f17
Omar Khan <omakhan98@gmail.com>
2023-08-24 15:42:18 +0500

    added add, and delete functionality using firebase

--- src/components/DataDisplayComponent.js
+++ src/components/DataDisplayComponent.js
@@ -1,12 +1,51 @@
 import React from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 import "./DataDisplayComponent.css";
+
 import CheckIcon from "@mui/icons-material/Check";
 import ClearIcon from "@mui/icons-material/Clear";
+
 import SubmitButton from "./SubmitButton";
+import axios from "axios";
 
 const DataDisplayComponent = ({ numberData }) => {
+  const saveToDatabase = () => {
+    const newData = {
+      carrier: numberData.carrier,
+      country: numberData.country,
+      country_code: numberData.country_code,
+      country_prefix: numberData.country_prefix,
+      data_fetching: numberData.data_fetching,
+      e164: numberData.e164,
+      international_number: numberData.international_number,
+      local_number: numberData.local_number,
+      phone_type: numberData.phone_type,
+      phone_valid: numberData.phone_valid,
+    };
+
+    axios
+      .post(
+        "https://phonenumvalid-default-rtdb.firebaseio.com/numbers.json",
+        newData
+      )
+      .then((response) => {
+        console.log("Data saved successfully:", response);
+        toast.success("Data stored successfully", {
+          bodyClassName: "toastify-success",
+          progressClassName: "toastify-progress-success",
+        });
+      })
+      .catch((error) => {
+        console.error("Error saving data:", error);
+        toast.error("Data storing failed", {});
+      });
+  };
+
   return (
     <div className="centered-listed-data">
+      <ToastContainer />
       <div className="listed-data">
         <div className="group-data">
           <span className="data-text">PHONE VALID</span>
@@ -51,7 +90,9 @@ const DataDisplayComponent = ({ numberData }) => {
           <span className="data-value">{numberData.phone_type}</span>
         </div>
         <div className="store-button">
-          <SubmitButton variant="outlined">Store</SubmitButton>
+          <SubmitButton onClick={saveToDatabase} variant="outlined">
+            Store
+          </SubmitButton>
         </div>
       </div>
     </div>
--- src/pages/StoredNumbers.js
+++ src/pages/StoredNumbers.js
@@ -1,10 +1,82 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./StoredNumbers.css";
+import axios from "axios";
+
+import NumbersFromDB from "../components/NumbersFromDB";
+import SubmitButton from "../components/SubmitButton";
+
+import Backdrop from "@mui/material/Backdrop";
+import CircularProgress from "@mui/material/CircularProgress";
+import { Link } from "react-router-dom";
 
 const StoredNumbers = () => {
+  const [loadedNumbers, setLoadedNumbers] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const getStoredNumbers = async () => {
+      try {
+        setLoading(true);
+        const response = await axios.get(
+          "https://phonenumvalid-default-rtdb.firebaseio.com/numbers.json"
+        );
+
+        const tempLoadedNumbers = [];
+
+        for (const key in response.data) {
+          tempLoadedNumbers.push({
+            id: key,
+            carrier: response.data[key].carrier,
+            country: response.data[key].country,
+            country_code: response.data[key].country_code,
+            country_prefix: response.data[key].country_prefix,
+            data_fetching: response.data[key].data_fetching,
+            e164: response.data[key].e164,
+            international_number: response.data[key].international_number,
+            local_number: response.data[key].local_number,
+            phone_type: response.data[key].phone_type,
+            phone_valid: response.data[key].phone_valid,
+          });
+        }
+
+        setLoadedNumbers(tempLoadedNumbers); // data saved from the helper function
+      } catch (e) {
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getStoredNumbers();
+  }, []);
+
   return (
     <div>
+      <div className="back-to-home">
+        <Link to="/">
+          <SubmitButton variant="outlined">Back</SubmitButton>
+        </Link>
+      </div>
       <h1 className="stored_numbers-title">StoredNumbers</h1>
+      {loadedNumbers.map((nums) => (
+        <NumbersFromDB
+          key={nums.id}
+          numData={nums}
+          onDelete={(deletedId) => {
+            setLoadedNumbers((prevLoadedNumbers) =>
+              prevLoadedNumbers.filter((num) => num.id !== deletedId)
+            );
+          }}
+        />
+      ))}
+      {/* Conditionally render the Backdrop and CircularProgress */}
+      {loading && (
+        <Backdrop
+          sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+          open={loading}
+        >
+          <CircularProgress color="inherit" />
+        </Backdrop>
+      )}
     </div>
   );
 };
